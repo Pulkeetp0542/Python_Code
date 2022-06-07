549fa9405fb5eac6f3446c97da3ae823d89221b4
Pulkeet.p0542 <pulkeet.p@outlook.com>
2022-06-07 13:36:58 +0530

    Nested If-Else

--- ifelse.py
+++ ifelse.py
@@ -1,7 +1,12 @@
 print("Welcome to the rollercoaster!!")
 height = int(input("What is your height in cm?"))
 
-if height == 120:
+if height >= 120:
     print("You can ride the rollercoaster")
+    age = int(input("Enter you age"))
+    if age >= 18:
+        print("Eligible for ride")
+    else:
+        print("Not Eligible")
 else:
     print("Sorry,You are not eligible for the ride")
