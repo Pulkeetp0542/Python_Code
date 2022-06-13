076f59683c2dae5ee0612602e81d1b33cdd997c9
Pulkeet.p0542 <pulkeet.p@outlook.com>
2022-06-13 17:06:51 +0530

    Addition and square using Numpy

---
+++ nnp.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+x=np.array([1,2,3])
+y=np.array([1,2,3])
+
+#addition of array
+sum=(x+y)
+
+print("Sum of x and y is",sum)
+
+#Square of array
+arr=np.array([10,5,1])
+print("square of array is ",np.square(arr))
